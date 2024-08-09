ad39e2339b49bda61c5e4fb877d919111366b3fc
Bazee-code <eugeneobazee2@gmail.com>
2024-08-09 13:16:14 +0300

    Check account entries in transfer transaction

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -3,6 +3,8 @@ package db
 import (
 	"context"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestTransferTransaction(t *testing.T) {
@@ -15,10 +17,12 @@ func TestTransferTransaction(t *testing.T) {
 	n := 5
 	amount := int64(10)
 
+	// we use channels here to make sure our tests are running in the main go routine
 	errs := make(chan error)
 	results := make(chan TransferTxResult)
 
 	for i := 0; i < n; i++ {
+		// new go routine
 		go func() {
 			result, err := store.TransferTx(context.Background(), TransferTxParams{
 				FromAccountID: account1.ID,
@@ -26,7 +30,31 @@ func TestTransferTransaction(t *testing.T) {
 				Amount:        amount,
 			})
 
+			errs <- err
+			results <- result
 		}()
+	}
+
+	// check results
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+
+		result := <-results
+		require.NotEmpty(t, result)
+
+		// check all obj in transfer obj
+		transfer := result.Transfer
+		require.NotEmpty(t, transfer)
+		require.Equal(t, account1.ID, transfer.FromAccountID)
+		require.Equal(t, account2.ID, transfer.ToAccountID)
+		require.Equal(t, amount, transfer.Amount)
+		require.NotZero(t, transfer.ID)
+		require.NotZero(t, transfer.CreatedAt)
+
+		_, err = store.GetTransfer(context.Background(), transfer.ID)
+		require.NoError(t, err)
+
 	}
 
 }
