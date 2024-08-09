82caa98ae41866363fc463c3aa89786ca7765d9e
Bazee-code <eugeneobazee2@gmail.com>
2024-08-09 12:24:47 +0300

    Add db transaction functions, finalize on createTransfer logic

---
+++ db/sqlc/store.go
@@ -0,0 +1,79 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// Store provides all funcs to execute db queries and transactions
+type Store struct {
+	*Queries // this is known as composition & is the preferred way of extending structs instead of inheritance
+
+	db *sql.DB //we add the db obj to give us access to db commands required to make a transaction
+}
+
+// NewStore creates a new store
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+// execTx executes a function within a db transaction
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		// rbErr - rollback error
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err :%v", err, rbErr)
+		}
+		return err
+	}
+	return tx.Commit()
+}
+
+// input params of the transfer transaction
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+// TransferTx perfroms a money transfer from one acc to another
+// It creates a transfer record, add acc entries and update acc balances with a single db transaction
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+
+	})
+
+	return result, err
+}
