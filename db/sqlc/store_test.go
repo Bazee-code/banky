e42b732ba4ea75d02848ee2163661c2e5cb3fcac
Bazee-code <eugeneobazee2@gmail.com>
2024-08-13 10:46:58 +0300

    Add testTransferDeadlock unit tests

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -127,3 +127,66 @@ func TestTransferTransaction(t *testing.T) {
 	require.Equal(t, account2.Balance+int64(n)*amount, updatedAccount2.Balance)
 
 }
+
+func TestTransferTransactionDeadlock(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	fmt.Println(">> before:", account1.Balance, account2.Balance)
+
+	// run n concurrent transfer transactions
+	// n := 2
+	n := 10
+	amount := int64(10)
+
+	// we use channels here to make sure our tests are running in the main go routine
+	errs := make(chan error)
+	// results := make(chan TransferTxResult)
+
+	for i := 0; i < n; i++ {
+		// txName := fmt.Sprintf("tx %d", i+1)
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		// half transactions to be from acc 2 to 1
+		if i%2 == 1 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
+
+		// new go routine
+		go func() {
+			// ctx := context.WithValue(context.Background(), txKey, txName)
+
+			_, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
+				Amount:        amount,
+			})
+
+			errs <- err
+			// results <- result
+		}()
+	}
+
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+
+	}
+
+	// check the final updated balances
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	fmt.Println(">> after:", updatedAccount1.Balance, updatedAccount2.Balance)
+
+	require.Equal(t, account1.Balance, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance, updatedAccount2.Balance)
+
+}
