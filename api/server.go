423414ea1d4d0ee3ad4d0a180466a97673f46491
Bazee-code <eugeneobazee2@gmail.com>
2024-08-14 12:42:35 +0300

    Add server file and account routes file

---
+++ api/server.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/techschool/simplebank/db/sqlc"
+)
+
+type Server struct {
+	store  *db.Store   //allow us to interact with db
+	router *gin.Engine // allows us to process our http requests
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/", server.createAccount)
+
+	server.router = router
+	return server
+}
+
+// run the http server on a specified address
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{
+		"error": err.Error()}
+}
